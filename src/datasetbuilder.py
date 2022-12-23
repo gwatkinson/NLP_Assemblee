8c49414ba0300377072113d9b193a21891c55a62
Jeremstym <jeremstym@gmail.com>
2022-12-23 17:29:21 +0100

    create dataset for deputies

--- src/datasetbuilder.py
+++ src/datasetbuilder.py
@@ -3,8 +3,8 @@
 import pandas as pd
 import numpy as np
 
-from cpc_api.api import CPCApi
-# sometimes, just use cpc_api.api when it works
+from src.cpc_api.api import CPCApi
+# sometimes, use src.cpc_api.api or just cpc_api.api
 # ignore the warning
 
 # load deputies dataframe
@@ -22,6 +22,15 @@ def deputies_of_group(group, n_deputies):
 
 
 def interventions_of_group(group, n_deputies=15):
+    """
+    Return a list of 50 interventions by deputy of one group.
+    Presented: acronym of the group, name of deputy, 50 interventions (list of str)
+    Args: group (str) Acronym of the group
+        n_deputies: (int) max number of selected deputies in the group. 
+        no problem if it is larger than the number of deputies
+    
+    Return: a 3 column dataframe [group, deputy's name, 50 interventions]
+    """
     names = deputies_of_group(group, n_deputies)
     print(names)
     interventions = []
@@ -31,6 +40,12 @@ def interventions_of_group(group, n_deputies=15):
     return interventions
 
 def stockintervention(groupe):
+    """
+    Same as above but without limit of the number of deputies
+    (takes them all). Could be a long process
+
+    Return: a 3 column dataframe [group, deputy's name, 50 interventions]
+    """
     interventions_group = []
     nbdep = deputies_df.groupby('groupe_sigle')['nom'].count()[str(groupe)]
     print(nbdep)
@@ -41,3 +56,17 @@ def stockintervention(groupe):
         )
     
     return interventions_df
+
+
+#### This part to create a huge dataframe with all deputies, all groups
+# and 50 interventions for each 
+# WARNING : take a long time, do not run it unless necessary
+
+# interventions_df = pd.DataFrame(columns=["groupe", "nom", "interventions"])
+
+# for groupe in groupes:
+#     interventions_df = pd.concat([interventions_df, stockintervention(groupe)], ignore_index=True)
+
+# path = "mypath"
+# interventions_df.to_csv(path + "\depinter.csv",index=False)
+
